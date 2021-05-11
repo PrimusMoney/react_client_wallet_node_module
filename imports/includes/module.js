898775352403a7ba4d3eb26a2c41487cf85eda86
fbaud <fbaud@p2pventure.org>
2021-05-11 17:52:59 +0200

    version 0.30.3

--- imports/includes/module.js
+++ imports/includes/module.js
@@ -5,7 +5,7 @@ var Module = class {
 	
 	constructor() {
 		this.name = 'mvc';
-		this.current_version = "0.30.2.2021.05.15";
+		this.current_version = "0.30.3.2021.05.15";
 		
 		this.global = null; // put by global on registration
 		this.app = null;
